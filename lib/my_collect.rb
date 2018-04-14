126bf26e3b2fd0db35d9e8a8538972118edc48f7
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-14 17:40:24 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,38 @@
 
-
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  new = []
+  index = 0 
+  while index < self.length 
+  new[index] = yield self[index]
+  index += 1
+end
+new
+end
+collection.my_collect {|lang| lang.upcase }
+array.my_collect {|name| name.split(" ").first }
