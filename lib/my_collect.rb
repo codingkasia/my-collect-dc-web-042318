cebbde25b81675cf39c987cc202403061b7bf69b
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-15 04:35:59 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -14,7 +14,17 @@ my_collect(collection) { |lang| lang.upcase }
 
   
   
-  
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+def my_collect(arr)
+  index = 0 
+  while index < arr.length
+  yield arr[index]
+  index += 1 
+end
+end
+my_collect(array) { |name| name.split(" ").first }
+my_collect(collection) { |lang| lang.upcase }
   
   
   
