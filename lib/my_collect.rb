080f6e76c46675ccde7fbd80d649d873ad91a528
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-15 04:37:58 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -51,14 +51,3 @@ my_collect(collection) { |lang| lang.upcase }
   
   
   
-  
-  new = []
-  index = 0 
-  while index < self.length 
-  new[index] = yield self[index]
-  index += 1
-end
-new
-end
-collection.my_collect {|lang| lang.upcase }
-array.my_collect {|name| name.split(" ").first }
